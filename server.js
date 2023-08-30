f0bcfcaafdbeb795a2115c9bb4c9ab5f4cd418e8
Rahimah <rahimeansari7@gmail.com>
2023-08-30 07:15:25 +0430

    3th party middleware: morgan

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 app.use(express.json());
 
 app.use(express.json());
+app.use(morgan('dev'))
 
 app.use((req, res, next) => {
   console.log('Hello from middleware!');
