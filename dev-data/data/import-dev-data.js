259556869d86927b07a50195c7a9e133abe91409
Rahimah Ansari <rahimeansari7@gmail.com>
2023-09-23 14:25:53 +0430

    implemented development data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+require('dotenv').config;
+const Tour = require('../../model/tourSchema');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.LOCAL_DATABASE);
+
+    console.log(`MongoDB Connected successfully!`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+connectDB();
+
+// READ JOSN FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data loaded successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany({}, { timeout: 30000 });
+    console.log('Data deleted successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
