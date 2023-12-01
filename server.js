4771f7b5834ca5a3d844dfdfd0a0588ea89b0b8a
Rahimah Ansari <rahimeansari7@gmail.com>
2023-12-01 08:07:10 +0430

    Handling Unhandled Routes

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.js`)
 // );
@@ -23,6 +22,24 @@ const tours = require("./route/tours/tour");
 const users = require("./route/users/user");
 app.use("/api/v1/tours", tours);
 app.use("/api/v1/users", users);
+
+app.all("*", (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
