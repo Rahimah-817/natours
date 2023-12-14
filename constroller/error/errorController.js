cfbbd745c4965b833aac8b2e0de952dee17b2d55
Rahimah Ansari <rahimeansari7@gmail.com>
2023-12-14 22:51:43 +0430

    Handling Mongoose Validation Errors

--- constroller/error/errorController.js
+++ constroller/error/errorController.js
@@ -1,16 +1,19 @@
 const AppError = require("../../utils/appError");
 
-
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.valid}.`;
   return new AppError(message, 400);
 };
-const handleDoblicateFieldsDB = err =>{
-  const value = err.errmsg.match(/(["'])(\\?.) *?\1/)
-  console.log(value)
-  const message = `Doblicat field value: ${value}. Please use another value!`
-  return new AppError(message, 400)
-}
+const handleDoblicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.) *?\1/);
+  const message = `Doblicat field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -45,7 +48,8 @@ const globalErrorHandler = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     if (error.name === "castError") error = handleCastErrorDB(error);
-    if(err.code === 11000) error=handleDoblicateFieldsDB(error)
+    if (err.code === 11000) error = handleDoblicateFieldsDB(error);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
     sendErrorPro(error, res);
   }
 };
