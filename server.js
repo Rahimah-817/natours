3837bf76a296a4596d76ce6e867b751ad6a88009
Rahimah Ansari <rahimeansari7@gmail.com>
2024-06-10 19:45:08 +0430

    prevent parameter pollution

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./constroller/error/errorController");
@@ -47,6 +48,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingQuantity",
+      "ratingAverage",
+      "difficulty",
+      "maxGroupSize",
+      "price",
+    ],
+  }),
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
