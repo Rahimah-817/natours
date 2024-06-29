4e73584070d8ba1fc587d96c3271a1adc28c3502
Rahimah Ansari <rahimeansari7@gmail.com>
2024-06-29 22:02:12 +0430

    Adding missing Authentication and Authorization

--- route/tour.js
+++ route/tour.js
@@ -15,11 +15,18 @@ const reviewRouter = require("./review");
 
 const router = express.Router();
 
-router.route("/").post(createTour).get(getAllTours);
-router.route("/:id").get(getTour).patch(updateTour).delete(protect, deleteTour);
+router
+  .route("/")
+  .post(protect, restrictTo("admin", "lead-guide"), createTour)
+  .get(getAllTours);
+router
+  .route("/:id")
+  .get(getTour)
+  .patch(protect, restrictTo("admin", "lead-guide"), updateTour)
+  .delete(protect, restrictTo("admin", "lead-guide"), deleteTour);
 router.route("/top-5-cheap").get(aliesTopTours, getAllTours);
 router.route("/tour-stats").get(getTourState);
-router.route("/monthly-plan/:year").get(getMonthlyPlan);
+router.route("/monthly-plan/:year").get(protect, restrictTo("admin", "lead-guide", 'guide'),getMonthlyPlan);
 
 router.use("/:tourId/reviews", reviewRouter);
 
