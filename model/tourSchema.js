6fea67e7c170a72c603aac0af4b982e9095531f7
Rahimah Ansari <rahimeansari7@gmail.com>
2024-07-02 16:47:44 +0430

    Preventing Duplicate Review

--- model/tourSchema.js
+++ model/tourSchema.js
@@ -35,6 +35,9 @@ const tourSchema = new mongoose.Schema(
     ratingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingQuantity: {
       type: Number,
