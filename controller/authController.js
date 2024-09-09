7b4df529bd416db3b3e244bee7b8b8a09f3eb01b
Rahimah Ansari <rahimeansari7@gmail.com>
2024-09-09 11:23:36 +0430

    Logging in users with our API 3

--- controller/authController.js
+++ controller/authController.js
@@ -110,27 +110,28 @@ const protect = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no errors!
-const isLoggedIn = catchAsync(async (req, res, next) => {
-  if (req.cookies.jwt) {
-    // 1) verify token
-    const decoded = await promisify(jwt.verify)(
-      req.cookies.jwt,
-      process.env.JWT_SECRET,
-    );
+const isLoggedIn = catchAsync(async (req, res, next) => {  
+  // Check if req.cookies is defined  
+  if (req.cookies && req.cookies.jwt) {  
+    // 1) verify token  
+    const decoded = await promisify(jwt.verify)(  
+      req.cookies.jwt,  
+      process.env.JWT_SECRET,  
+    );  
 
-    // 2) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) return next();
+    // 2) Check if user still exists  
+    const currentUser = await User.findById(decoded.id);  
+    if (!currentUser) return next();  
 
-    // 3) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.validateBeforeSave))
-      return next();
+    // 3) Check if user changed password after the token was issued  
+    if (currentUser.changedPasswordAfter(decoded.validateBeforeSave))  
+      return next();  
 
-    // THERE IS A LOGGED IN USER
-    res.locals.user = currentUser;
-    return next()
-  }
-  next()
+    // THERE IS A LOGGED IN USER  
+    res.locals.user = currentUser;  
+    return next();  
+  }  
+  next();  
 });
 
 const restrictTo = (...roles) => {
